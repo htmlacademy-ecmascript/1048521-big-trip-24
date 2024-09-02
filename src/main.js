a9db7ad1f17ca4513bb65def2e4988a238155dd0
Kiamova Elvira <ela.kiyamova@yandex.ru>
2024-09-02 18:29:04 +0500

    feat: add a drawing of the sorting form

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 
+import {render, RenderPosition} from './render.js';
 import FilterView from './view/filter-view.js';
 import RoutePointView from './view/route-point-view.js';
-import {render, RenderPosition} from './render.js';
+import SortingView from './view/sorting-view.js';
+
 
 const siteControlsFilters = document.querySelector('.trip-controls__filters');
+const siteEvents = document.querySelector('.trip-events');
 
 render(new RoutePointView(), siteControlsFilters, RenderPosition.AFTERBEGIN);
 render(new FilterView(), siteControlsFilters);
+render(new SortingView(), siteEvents);
 
